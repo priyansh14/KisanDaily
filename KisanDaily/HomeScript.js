88f695bf960174783073e09c3a5449094789a2ec
Rupender-Bhati <40535865+Rupender-Bhati@users.noreply.github.com>
2018-10-21 02:44:36 +0530

    Add files via upload

---
+++ KisanDaily/HomeScript.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	'use strict';
+	
+	var controller = new ScrollMagic.Controller();
+	
+	
+	var Sike = new ScrollMagic.Scene({
+		
+		triggerElement: '.stickyLegend'
+		
+		
+		
+		
+	})
+	
+	.setPin('.stickyLegend')
+	.addTo(controller)
+	.addIndicators();
+
+	
+});
